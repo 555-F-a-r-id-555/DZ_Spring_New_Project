3ae9f583946e109e291add6d9847688b25f247ee
Farid <makflay85zet@gmail.com>
2024-07-02 01:20:56 +0400

    DZ2 V3 Scoreboard fixed

--- myFirstProject/src/main/java/com/example/myFirstProject/V3/Scoreboard.java
+++ myFirstProject/src/main/java/com/example/myFirstProject/V3/Scoreboard.java
@@ -15,9 +15,9 @@ public class Scoreboard {
         LocalDateTime localDateTime = LocalDateTime.now();
         return new Ticket(ticketNumber, localDateTime);
     }
-    public com.example.myFirstProject.V2.Ticket newTicket2() {
+    public Ticket newTicket2() {
         String ticketNumber = ticketNumberGenerator.createNewNumber2();
         LocalDateTime localDateTime = LocalDateTime.now();
-        return new com.example.myFirstProject.V2.Ticket(ticketNumber, localDateTime);
+        return new Ticket(ticketNumber, localDateTime);
     }
 }
