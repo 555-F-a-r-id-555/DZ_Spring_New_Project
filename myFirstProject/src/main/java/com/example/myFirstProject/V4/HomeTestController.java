5ee9d809a3d142ffd0f543a6edda10db78eb03b7
Farid <makflay85zet@gmail.com>
2024-07-02 22:40:36 +0400

    DZ2 V4 new files added

--- myFirstProject/src/main/java/com/example/myFirstProject/V4/HomeTestController.java
+++ myFirstProject/src/main/java/com/example/myFirstProject/V4/HomeTestController.java
@@ -1,30 +1,36 @@
 package com.example.myFirstProject.V4;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-public class HomeController {
+@Controller
+public class HomeTestController {
+
+//    @GetMapping("/home-test")
+//    public String test() {
+//        return "/home-test.html";
+//    }
+
     private final Scoreboard scoreboard;
 
     @Autowired
-    public HomeController(Scoreboard scoreboard) {
+    public HomeTestController(Scoreboard scoreboard) {
         this.scoreboard = scoreboard;
     }
 
-    @GetMapping("/home")
-    public List<Ticket> home(@RequestParam(value = "type", defaultValue = "regular") String type) {
+    @GetMapping("/home-test")
+    public String test(@RequestParam(value = "type", defaultValue = "regular") String type, Model model) {
         List<Ticket> tickets = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
             tickets.add(scoreboard.newTicket(type));
         }
-        return tickets;
+        model.addAttribute("tickets", tickets);
+        return "home-test";
     }
-
 }
