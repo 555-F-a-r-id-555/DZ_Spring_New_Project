3789bb903c900c719a271e96d7deb8e68c80fa21
Farid <makflay85zet@gmail.com>
2024-07-02 21:44:14 +0400

    DZ2 V4 added

--- myFirstProject/src/main/java/com/example/myFirstProject/V1/Main1.java
+++ myFirstProject/src/main/java/com/example/myFirstProject/V1/Main1.java
@@ -29,10 +29,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class MyFirstProjectApplication {
+public class Main1 {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(MyFirstProjectApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Main1.class, args);
         for (int i = 1; i < 6; i++) {
             System.out.println(context.getBean(Scoreboard.class).newTicket());
             System.out.println(context.getBean(Scoreboard.class).newTicket2());
